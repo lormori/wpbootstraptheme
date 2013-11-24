b9c85baaf6b130073d7706fc45cb6a28fdb4eeff
Lorenzo Mori <lor.mori@gmail.com>
2013-11-24 20:40:46 +0000

    resized the width of blog and single pages.

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
 <div class="container">
 
 <div class = "row">
-	<div class="span8">	
+	<div class="span8 blog">	
 		<h1> Blog </h1>
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
