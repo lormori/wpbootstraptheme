6a18c55f168a53d09d4e351e407b9a0c132cb355
Lorenzo Mori <lor.mori@gmail.com>
2013-11-27 00:24:08 +0000

    added footer ideas as strings. need to actually do that

--- footer.php
+++ footer.php
@@ -1,11 +1,21 @@
 	<hr>
 	<!-- mega footer -->
-	<footer>
-		<p>Lorenzo Mori&trade;</p>
-	</footer>
+	
 	
 	</div> <!-- container from header.php -->
     
+	<footer>
+		<div class="big-divider">
+			<p>Lorenzo Mori&trade;</p>
+			
+			<p> say what this site is about </p>
+			<p> put a link to home </p>
+			<p> put the Mori japanese character </p>
+			<p> some social icons? github, fb, linkedin? </p>
+			<p> email? </p>
+		</div>
+	</footer>
+	
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <!-- commentin out the js stuff that is not needed (at the moment.. ? 
 	<script src="//code.jquery.com/jquery.js"></script> -->
